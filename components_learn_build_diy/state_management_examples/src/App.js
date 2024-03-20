0abeabfcd6f9905af789478ecdf83e40049e0a24
Linh Huynh <cdev.linh@gmail.com>
2024-03-19 22:58:56 -0700

    Updated ToDoList_useReducer

--- components_learn_build_diy/state_management_examples/src/App.js
+++ components_learn_build_diy/state_management_examples/src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import ToDoList_UseState from "./components/state_examples/UseState_ex/To-do_List/ToDoList_UseState";
-import { ToDoList_useReducer } from "./components/state_examples/useReducer_ex/To-do_List/ToDoList_useReducer";
+import ToDoList_useReducer from "./components/state_examples/useReducer_ex/To-do_List/ToDoList_useReducer";
+
+// import { ToDoList_useReducer } from "./components/state_examples/useReducer_ex/To-do_List/ToDoList_useReducer";
 
 function App() {
   return (
@@ -8,7 +10,7 @@ function App() {
       {/*  */}
 
       {/* https://react.dev/learn/extracting-state-logic-into-a-reducer?fbclid=IwAR30XfkCFMIpTcmB-4zNxXxUYJyrilNXN0WRoU0dacBot13PmSqITZrZqQI */}
-      <ToDoList_UseState />
+      {/* <ToDoList_UseState /> */}
 
       <ToDoList_useReducer />
 
