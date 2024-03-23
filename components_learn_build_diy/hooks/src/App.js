f35b4f14149e1eabbcc89d5f238ed5bf4ef4eb22
Linh Huynh <cdev.linh@gmail.com>
2024-03-23 02:11:58 -0700

    Demo UseMemo3 using useMemo()

--- components_learn_build_diy/hooks/src/App.js
+++ components_learn_build_diy/hooks/src/App.js
@@ -11,7 +11,7 @@ function App() {
 
       {/* useMemo() */}
       {/* <UseMemo /> */}
-      <UseMemo2 />
+      {/* <UseMemo2 /> */}
       <UseMemo3 />
 
       {/*  */}
--- components_learn_build_diy/hooks/src/components/hooks-demo/useMemo-demo/demo-3/UseMemo3.js
+++ components_learn_build_diy/hooks/src/components/hooks-demo/useMemo-demo/demo-3/UseMemo3.js
@@ -1,5 +1,36 @@
-import React from "react";
+import React, { useState, useMemo } from "react";
+
+function expensiveFunction(number) {
+  console.log("time start");
+  const start = new Date();
+
+  while (new Date() - start < 3000);
+
+  console.log("end ", new Date() - start, "ms");
+
+  return number * number;
+}
 
 export default function UseMemo3() {
-  return <div>UseMemo3</div>;
+  const [count, setCount] = useState(0);
+  const [num, setNum] = useState(20);
+
+  const number = useMemo(() => {
+    return expensiveFunction(num);
+  }, [num]);
+
+  //   console.log(count);
+
+  return (
+    <div>
+      <h2>UseMemo3</h2>
+
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Add</button>
+      <button onClick={() => setNum(Math.random())}>Increase</button>
+
+      <p>Number: {number}</p>
+      <p>Random Number: {num}</p>
+    </div>
+  );
 }
